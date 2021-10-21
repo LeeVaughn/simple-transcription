82607a78df931d572252e648d1fae59e25f53bae
LeeVaughn <dlvprogramming@gmail.com>
2021-10-21 14:27:50 -0700

    Add functions to display results and error and to call the getTranscription function

--- js/script.js
+++ js/script.js
@@ -39,3 +39,36 @@ async function getTranscription(url="") {
   });
   return response.json();
 }
+
+/**
+ * Displays error message
+ * 
+ * @param {string} msg - error message to display
+ */
+ function displayError(msg) {
+  const errorMessage = `<p class="error">${msg}</p>`;
+
+  document.querySelector("main").innerHTML = errorMessage;
+}
+
+/**
+ * Displays transcription results
+ * 
+ * @param {string} text - transcribed text from the audio file
+ */
+ function displayResults(text) {
+  console.log(text)
+}
+
+getTranscription("https://api.assemblyai.com/v2/transcript/fi0wyq92h-d790-43dc-8385-01bfac11031f")
+.then(data => {
+  if (data.status === "processing") {
+    displayError("The data is still processing. Please try again in a moment.");
+  } else {
+    transcribedText = data.text;
+    displayResults(transcribedText);
+  }
+})
+.catch((err) => console.error(err));
+
+
